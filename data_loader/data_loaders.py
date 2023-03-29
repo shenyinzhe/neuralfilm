d23d37314a97439718d3a54c2d16d370470f05ac
shenyinzhe <shenyinzhe.de@outlook.com>
2023-03-29 12:14:13 +0200

    Add augmentation; set img size to 200; set batch_size to 16

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from PIL import Image
 from torchvision import datasets, transforms
 from torch.utils.data import Dataset
@@ -7,18 +8,21 @@ from base import BaseDataLoader
 
 class DataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize([800,800])
-        ])
         self.data_dir = data_dir
-        self.dataset = HasselbladDataset(self.data_dir, trsfm)
+        self.dataset = HasselbladDataset(self.data_dir)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
 class HasselbladDataset(Dataset):
-    def __init__(self, data_dir, transform):
-        self.transform = transform
+    def __init__(self, data_dir):
+        self.transform = transforms.Compose([
+            transforms.RandomAffine(180,shear=45),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomCrop([200,200]),
+        ])
+
+        self.to_tensor = transforms.ToTensor()
         self.input_dir = os.path.join(data_dir, "input")
         self.label_dir = os.path.join(data_dir, "label")
         self.images = sorted(os.listdir(self.input_dir))
@@ -26,11 +30,13 @@ class HasselbladDataset(Dataset):
 
     def __getitem__(self, index):
         image = Image.open(os.path.join(self.input_dir, self.images[index]))
+        image = self.to_tensor(image)
         target = Image.open(os.path.join(self.label_dir, self.target[index]))
-        image = self.transform(image)
-        target = self.transform(target)
+        target = self.to_tensor(target)
+        data = torch.stack([image, target], dim = 0)
+        data = self.transform(data)
 
-        return image, target
+        return data[0], data[1]
 
     def __len__(self):
         return len(self.images)
