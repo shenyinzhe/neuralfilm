e0a10bc15fccee44657eef543a91dec13c6e0c88
shenyinzhe <shenyinzhe.de@outlook.com>
2023-03-29 21:40:59 +0200

    Save every 5 epochs

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -116,8 +116,9 @@ class BaseTrainer:
             'config': self.config
         }
         filename = str(self.checkpoint_dir / 'checkpoint-epoch{}.pth'.format(epoch))
-        torch.save(state, filename)
-        self.logger.info("Saving checkpoint: {} ...".format(filename))
+        if epoch % 5 == 1:
+            torch.save(state, filename)
+            self.logger.info("Saving checkpoint: {} ...".format(filename))
         if save_best:
             best_path = str(self.checkpoint_dir / 'model_best.pth')
             torch.save(state, best_path)
